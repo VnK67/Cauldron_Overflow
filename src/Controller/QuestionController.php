d80c1cfffa430cee0ac5e4ae3b206cfbcaecbdcc
Kev <hkev1@hotmail.fr>
2022-03-11 20:16:02 +0100

    Controller and first routes

--- src/Controller/QuestionController.php
+++ src/Controller/QuestionController.php
@@ -14,6 +14,17 @@ class QuestionController
      */
     public function homepage()
     {
-        return new Response('hello');
+        return new Response('What a bewitching controller we have conjured!');
+    }
+
+    /**
+     * @Route("/questions/{slug}", name="show")
+     */
+    public function show(string $slug)
+    {
+        return new Response(sprintf(
+            'Future page to show the question "%s"!',
+            ucwords(str_replace('-', ' ', $slug))
+        ));
     }
 }
